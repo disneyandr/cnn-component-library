e26b51c848e7daba181fbff261e8842ddf8c102f
Disney <disneyandrade@gmail.com>
2024-11-26 08:58:00 -0300

    alteração do hover do button secundário

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -7,5 +7,5 @@ export default {
     
 };
 
-export const Primario = () => <Button label="Primário!" onClick={() => alert('Clicou no botão primário!')} bgColor="bg-blue-500"></Button>;
-export const Secundario = () => <Button label="Secundário!" onClick={() => alert('Clicou no botão secundario!')} bgColor="bg-orange-500" ></Button>;
+export const Primario = () => <Button label="Primário!" onClick={() => alert('Clicou no botão primário!')} bgColor="bg-blue-500" hover="hover:bg-blue-600"></Button>;
+export const Secundario = () => <Button label="Secundário!" onClick={() => alert('Clicou no botão secundario!')} bgColor="bg-orange-500" hover="hover:bg-orange-800" ></Button>;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,11 +8,11 @@ interface ButtonProps {
     bgColor: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, bgColor }) => {
+const Button: React.FC<ButtonProps> = ({ label, onClick, bgColor, hover }) => {
     return (
         <button
             onClick={onClick}
-            className={`px-4 py-2 text-white font-bold rounded hover:bg-blue-600 ${bgColor}`}
+            className={`px-4 py-2 text-white font-bold rounded  ${bgColor} ${hover}`}
         >
             {label}
         </button>
