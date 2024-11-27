49b66e96b28d46517432ad64fafd8eb125128527
Disney <disneyandrade@gmail.com>
2024-11-27 10:13:22 -0300

    ajuste do main do package.json e build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
-import './globals.css';
+import 'globals.css';
 import Button from "./components/Button/Button";
 export { Button };
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,3 @@
-import './globals.css';
+import 'globals.css';
 import Button from "./components/Button/Button"; // Importação padrão
 export { Button };
